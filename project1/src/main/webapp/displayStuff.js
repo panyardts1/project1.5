0050e355be95e8a4d6ceb89cb5dd26ce922e43fb
panyardts1 <panyardts1@gmail.com>
2018-08-08 13:45:44 -0400

    stage

--- project1/src/main/webapp/displayStuff.js
+++ project1/src/main/webapp/displayStuff.js
@@ -20,7 +20,7 @@ function displayUserInfo(){
            document.getElementById("viewProfile").innerHTML = this.responseText;
         }
     };
-    xhttp.open("POST", "/project1/Profile.java", true);
+    xhttp.open("GET", "/project1/allReimbursments", true);
     xhttp.send(); 
 }
 
