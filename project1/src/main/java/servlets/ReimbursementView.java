c6b3e65204d2c631049a894180b341eb1d11a302
panyardts1 <panyardts1@gmail.com>
2018-08-10 10:02:21 -0400

    changes

--- project1/src/main/java/servlets/ReimbursementView.java
+++ project1/src/main/java/servlets/ReimbursementView.java
@@ -33,35 +33,36 @@ public class ReimbursementView extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession session = request.getSession();
-		//Employee e = EmployeeService.getEService().getEmployeeById((int)session.getAttribute("user"));
-		System.out.println((int)session.getAttribute("user"));
-		//List<Reimbursment> pendList = ReimbursmentService.getRService().viewEmployeePending(e.getU_ID());
-		//List<Reimbursment> resolveList = ReimbursmentService.getRService().viewEmployeeResolved(e.getU_ID());
-		
-		/*PrintWriter pw = response.getWriter();
-		pw.write("<!DOCTYPE html><html><head><meta charset=\"ISO-8859-1\"><title>View Profile</title>"
-				+ "</head><body>");
-		
-		String tableStart = "<table><tr><th>ID</th><th>Amount</th><th>Description</th><th>Date Submitted</th><th>Status</th><th>Manager ID</th></tr>";
-		String tableEnd = "</table>";
+		if(request != null) {
+			HttpSession session = request.getSession();
+			//Employee e = EmployeeService.getEService().getEmployeeById((int)session.getAttribute("user"));
+			System.out.println((int)session.getAttribute("user"));
+			//List<Reimbursment> pendList = ReimbursmentService.getRService().viewEmployeePending(e.getU_ID());
+			//List<Reimbursment> resolveList = ReimbursmentService.getRService().viewEmployeeResolved(e.getU_ID());
+			
+			/*PrintWriter pw = response.getWriter();
+			pw.write("<!DOCTYPE html><html><head><meta charset=\"ISO-8859-1\"><title>View Profile</title>"
+					+ "</head><body>");
+			
+			String tableStart = "<table><tr><th>ID</th><th>Amount</th><th>Description</th><th>Date Submitted</th><th>Status</th><th>Manager ID</th></tr>";
+			String tableEnd = "</table>";
 
-		pw.write(tableStart);
-		
-		//useless comment
-		
-		for(Reimbursment r : pendList) {
-			pw.write("<tr><td>" + r.getR_ID() + "</td><td>" + r.getAmount() + "</td><td>" + r.getDescription() + "</td><td>" + r.getDateSubmitted() + "</td><td>" + r.getStatus() + "</td><td>" + r.getM_ID() + "</td></tr>");
-		}
-		
-		for(Reimbursment r : resolveList) {
-			pw.write("<tr><td>" + r.getR_ID() + "</td><td>" + r.getAmount() + "</td><td>" + r.getDescription() + "</td><td>" + r.getDateSubmitted() + "</td><td>" + r.getStatus() + "</td><td>" + r.getM_ID() + "</td></tr>");
-		}
-		
-		pw.write(tableEnd);
+			pw.write(tableStart);
+			
+			//useless comment
+			
+			for(Reimbursment r : pendList) {
+				pw.write("<tr><td>" + r.getR_ID() + "</td><td>" + r.getAmount() + "</td><td>" + r.getDescription() + "</td><td>" + r.getDateSubmitted() + "</td><td>" + r.getStatus() + "</td><td>" + r.getM_ID() + "</td></tr>");
+			}
+			
+			for(Reimbursment r : resolveList) {
+				pw.write("<tr><td>" + r.getR_ID() + "</td><td>" + r.getAmount() + "</td><td>" + r.getDescription() + "</td><td>" + r.getDateSubmitted() + "</td><td>" + r.getStatus() + "</td><td>" + r.getM_ID() + "</td></tr>");
+			}
+			
+			pw.write(tableEnd);
 
-		pw.write("</body><script src=\"displayStuff.js\"></script></html>");*/
+			pw.write("</body><script src=\"displayStuff.js\"></script></html>");*/
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
